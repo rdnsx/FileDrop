18489080c51511be8f7e4915ee516455dca9d494
MGebremski <m.gebremski@steinberg.de>
2024-03-27 20:36:03 +0100

    update jenkins.groovy and templates/index.html

--- jenkins.groovy
+++ jenkins.groovy
@@ -11,19 +11,19 @@ pipeline {
         SSH_PORT = '22'
         WEBSITE_URL = 'https://drop2share.de'
         WAIT_TIME = 60
+
+        NTFY_SERVER = 'ntfy.rdnsx.de'
+        NTFY_TOPIC = 'RDNSX_Jenkins'
     }
     
     stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'main', url: env.SOURCE_REPO_URL
-            }
-        }
         
         stage('Build Docker Image') {
             steps {
                 script {
                     def buildNumber = env.BUILD_NUMBER
+                    sh "sed -i 's/{{BUILD_NUMBER}}/${buildNumber}/g' templates/index.html"
+
                     docker.withRegistry('', DOCKER_HUB_CREDENTIALS) {
                         def dockerImage = docker.build("${DOCKER_IMAGE_NAME}:${buildNumber}", ".")
                         dockerImage.push()
@@ -58,21 +58,30 @@ pipeline {
             }
         }
 
-        stage('Check Website Status') {
+        stage('Check Website Status and Notify') {
             steps {
                 script {
-
+                    def buildNumber = env.BUILD_NUMBER
+                    def ntfyServer = env.NTFY_SERVER
+                    def ntfyTopic = env.NTFY_TOPIC
+                    def websiteUrl = env.WEBSITE_URL
+                    
                     echo "Waiting for ${WAIT_TIME} seconds before checking website status..."
-                    sleep WAIT_TIME
+                    sleep time: WAIT_TIME.toInteger(), unit: 'SECONDS'
+                    
+                    def curlResponse = sh(script: "curl -s -o response.txt -w '%{http_code}' ${websiteUrl}", returnStdout: true).trim()
+                    def response = readFile('response.txt').trim()
 
-                    def response = sh(script: "curl -s ${WEBSITE_URL}", returnStdout: true).trim()
-
-                    if (response.contains('File')) {
-                        echo "Website is up and contains 'File'."
+                    if (curlResponse == '200' && response.contains(buildNumber)) {
+                        def message = "👍 ${websiteUrl} is successfully running on build ${buildNumber}!"
+                        echo message
+                        sh "curl -d '${message}' -H 'Actions: view, Check website, ${websiteUrl}' ${ntfyServer}/${ntfyTopic}"
                     } else {
-                        error "Website is not responding properly or does not contain 'File'."
+                        def errorMessage = "⛔️ ${websiteUrl} is not responding properly or does not contain ${buildNumber}!"
+                        echo errorMessage
+                        sh "curl -d '${errorMessage}' -H 'Actions: view, Check website, ${websiteUrl}' ${ntfyServer}/${ntfyTopic}"
+                        error errorMessage
                     }
-
                 }
             }
         }
